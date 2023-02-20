cddd4d762943a1826d362cade6860134263c6ad9
Kulpot <kulpotwahu@gmail.com>
2023-02-20 21:36:00 +0800

    Add project files.

---
+++ OOP_Properties/Box.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//          Incapsulation --using setter/getter/-----restricting the component of an objects by using access modifiers(private/public)
+namespace OOP_Properties
+{
+    internal class Box
+    {
+
+        // member variable
+        private int length = 3;
+        private int height;
+        //public int width; // instead use default - e.g.- public int Width { get; set; }
+        private int volume;
+
+        //PROPERTIES
+        public int Width { get; set; } // shorter and same as below(line 22-26) use for no logic in properties
+        /*public int Width
+        {
+            get { return this.width; }
+            set { this.width = value; }
+        }*/
+        
+        public int Volume
+        {
+            get
+            {
+                //return volume; 
+                return this.length * this.height * this.Width;
+            }
+           /* set
+            {
+                this.volume = value;
+            }*/
+        }
+        //Logic Properties
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                if(value < 0)//logic
+                {
+                    height = -value; ///if less than 0 then multiply by -1
+                }
+                else
+                {
+                    height = value;
+                }
+                
+            }
+
+        }
+
+        //constructor
+        public Box(int length, int height, int width)
+        {
+            this.length = length;
+            this.height = height;
+            Width = width;
+        }
+
+
+        //setter
+        public void SetLength(int length)
+        {
+            if(length < 0)
+            {
+                throw new Exception("Length should be higher than 0");
+            }
+            this.length = length;
+        }
+
+        //getter
+        public int GetLength()
+        {
+            return this.length; // this.length is the member variable length
+        }
+        /*
+        public int GetVolume()
+        {
+            return this.length * this.height * this.width;
+        }*/
+
+        public void DisplayInfo()
+        {
+            // Console.WriteLine("Length is {0} and height is {1} and width is {2} so the volume is {3}", 
+            //  length, height, width, volume = length*height*width);
+            Console.WriteLine("Length is {0} and height is {1} and width is {2} so the volume is {3}",
+                length, height, Width, volume = length * height * Width);
+        }
+
+
+    }
+}
---
+++ OOP_Properties/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOP_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box(3,4,5);// constructor set
+            //box.length = 3;
+            //box.SetLength(-4); //throw new Exception("Length should be higher than 0");
+            //box.SetLength(4);
+            //box.Height = -4;
+            //box.Width = 5;
+            Console.WriteLine("Box length is " + box.GetLength());
+            //Console.WriteLine("Box's volume is " + box.GetVolume());
+            Console.WriteLine("Box's volume is " + box.Volume);
+            Console.WriteLine("Box's width is " + box.Width);
+            box.Width = 10;
+            Console.WriteLine("Box's width is " + box.Width);
+            box.DisplayInfo();
+
+
+        }
+    }
+}
